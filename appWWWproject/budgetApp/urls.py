a51801f459d4cec996173ebdecbf533298e15424
PDondalski <73539016+PDondalski@users.noreply.github.com>
2023-12-09 14:35:40 +0100

    Zmiana adresu w urls.py

--- appWWWproject/budgetApp/urls.py
+++ appWWWproject/budgetApp/urls.py
@@ -6,6 +6,6 @@ from .views import BudgetDetail, BudgetList
 urlpatterns = [
     path("", views.budget_list, name='list'),
     path("<int:id>", views.budget_detail, name='detail'),
-    path('budget/<int:id>/', BudgetDetail.as_view(), name='budget-detail'),
+    path('budgets/<int:id>/', BudgetDetail.as_view(), name='budget-detail'),
     path('budgets/', BudgetList.as_view(), name='budget-list'),
 ]
