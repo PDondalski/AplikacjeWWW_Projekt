54e84ba72a98dcffe5bbdb7fb88935518f497a0f
PDondalski <73539016+PDondalski@users.noreply.github.com>
2023-12-09 14:24:26 +0100

    Dodany widok - BudgetDetail widoczny w rest API

--- appWWWproject/budgetApp/urls.py
+++ appWWWproject/budgetApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from budgetApp import views
 from . import views
+from .views import BudgetDetailView
 
 urlpatterns = [
     path("", views.budget_list, name='list'),
     path("<int:id>", views.budget_detail, name='detail'),
+    path('budget/<int:id>/', BudgetDetailView.as_view(), name='budget-detail'),
 ]
--- appWWWproject/budgetApp/views.py
+++ appWWWproject/budgetApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Sum
 from .models import *
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from .serializers import BgBudzetSerializer, BgWydatekSerializer
+from django.http import Http404
 
 def home(request):
     return render(request, "budgetApp/home.html", {})
@@ -15,3 +19,21 @@ def budget_detail(request, id):
     return render(request, "budgetApp/budget-detail.html",
                   {'budget': budget, 'wydatek_list': budget.wydatek.all(), 'total_wydatek': total_wydatek,
                    'oszczednosci': oszczednosci})
+
+class BudgetDetailView(APIView):
+    def get_object(self, id):
+        try:
+            return BgBudzet.objects.get(pk=id)
+        except BgBudzet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        budget = self.get_object(id)
+        total_wydatek = budget.wydatek.all().aggregate(Sum('wydatek_wartosc'))['wydatek_wartosc__sum']
+        oszczednosci = budget.budzet_wartosc - total_wydatek if total_wydatek else budget.budzet_wartosc
+        budget_serializer = BgBudzetSerializer(budget)
+        return Response({
+            'budget': budget_serializer.data,
+            'total_wydatek': total_wydatek,
+            'oszczednosci': oszczednosci
+        })
