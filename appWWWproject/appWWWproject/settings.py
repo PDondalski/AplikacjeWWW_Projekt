0db19b0c23ca84b5bfe55792a79ded769ce1f319
PDondalski <73539016+PDondalski@users.noreply.github.com>
2023-12-09 14:11:58 +0100

    Rest Framework import + TimeZone

--- appWWWproject/appWWWproject/settings.py
+++ appWWWproject/appWWWproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
